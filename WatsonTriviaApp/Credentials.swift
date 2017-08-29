5e1dea7728c803c1d56c2182e558033550a0910e
dbrandl <dbrandl@dbrandls-MBP.fios-router.home>
2017-08-28 21:29:23 -0400

    code commit

---
+++ WatsonTriviaApp/Credentials.swift
@@ -0,0 +1,27 @@
+/**
+ * Copyright IBM Corporation 2016, 2017
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+import Foundation
+
+/**
+ The credentials needed for the Watson APIs. 
+ */
+struct Credentials {
+    static let SpeechToTextUsername = "065b7b13-2eb8-448e-ab3d-18a624013edf" /// UPDATE WITH YOUR CREDENTIALS
+    static let SpeechToTextPassword = "alwpbiMdqLLR" /// UPDATE WITH YOUR CREDENTIALS
+    static let TextToSpeechUsername = "38d85c6f-e132-45cb-a8b8-bbd7ab42a804" /// UPDATE WITH YOUR CREDENTIALS
+    static let TextToSpeechPassword = "zE8Z2AdpHWMz" /// UPDATE WITH YOUR CREDENTIALS
+}
